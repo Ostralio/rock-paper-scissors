685c1741fd1107cfb1b6ba3a7766d57c4ec812d1
Siddharth Belwal <siddharth@siddharths-mbp.lan>
2022-12-27 21:17:03 -0800

    mwahahahaah

--- rpsModel.py
+++ rpsModel.py
@@ -8,12 +8,14 @@ import os
 import sys
 import json
 
+
+# setting up arrays of training data and testing data and stuff
+
 train_images = []
 train_labels = []
 test_images = []
 test_labels = []
 
-label_key = {'rock': 0, 'scissors': 1, 'paper': 2}
 result_key = {0: 'rock', 1: 'scissors', 2: 'paper'}
 
 train_paths = os.listdir('FaceTrainData/images')
@@ -29,31 +31,45 @@ for img_path in test_paths:
 
 with open('FaceTrainData/labels.json') as file:
         data = json.load(file)
+        for label in data.values():
+            train_labels.append(label)
         
-
 train_images_np = np.asarray(train_images)
 train_labels_np = np.asarray(train_labels)
 test_images_np = np.asarray(test_images)
 test_labels_np = np.asarray(train_labels)
 
-# model = keras.Sequential([
-#     keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=(48, 48, 1)),
-#     keras.layers.MaxPooling2D((2, 2), strides=2),
-#     # second convolution layer
-#     keras.layers.Conv2D(64, (3, 3), activation="relu"),
-#     keras.layers.MaxPooling2D((2, 2), strides=2),
+# the model
 
-#     keras.layers.Conv2D(128, (3, 3), activation="relu"),
-#     keras.layers.MaxPooling2D((2, 2), strides=2),
-#     # fully connected classification
-#     # single vector
-#     keras.layers.Flatten(),
-#     keras.layers.Dense(1024, activation="relu"),
-#     keras.layers.Dense(7, activation=tf.nn.softmax)    # Finds the greatest correlation to one of our labels 1-10
-# ])
+model = keras.Sequential([
+    keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=(48, 48, 1)),
+    keras.layers.MaxPooling2D((2, 2), strides=2),
+    # second convolution layer
+    keras.layers.Conv2D(64, (3, 3), activation="relu"),
+    keras.layers.MaxPooling2D((2, 2), strides=2),
 
-# model.compile(optimizer=tf.optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics='accuracy')
-# model.fit(train_images_np, train_labels_np, epochs=5)
+    keras.layers.Conv2D(128, (3, 3), activation="relu"),
+    keras.layers.MaxPooling2D((2, 2), strides=2),
+    # fully connected classification
+    # single vector
+    keras.layers.Flatten(),
+    keras.layers.Dense(1024, activation="relu"),
+    keras.layers.Dense(7, activation=tf.nn.softmax)    # Finds the greatest correlation to one of our labels 1-10
+])
+
+model.compile(optimizer=tf.optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics='accuracy')
+model.fit(train_images_np, train_labels_np, epochs=5)
+
+# code to plug in one image and see what the model thinks it is
+testImage = []
+
+prediction = model.predict(np.array([testImage]))
+print(prediction)
+pred2 = np.delete(prediction[0], np.array(np.argmax(prediction[0], axis=0)))
+print(f'This is a {result_key[np.argmax(prediction[0], axis=0)]} with a condfidence of {max(prediction[0])} versus the 2nd highest of {max(pred2)}')
+
+# code to test the model against a bunch of test images
+
 # test_loss, test_acc = model.evaluate(test_images_np, test_labels_np)
 # print(test_loss, test_acc)
 
