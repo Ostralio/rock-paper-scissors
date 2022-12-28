fba9ba00887108550a3641ed4b0d0b337eadd192
Henry <bednarzhenry@gmail.com>
2022-12-27 16:27:22 -0800

    cleanedsomestuff

--- RPSTools.py
+++ RPSTools.py
@@ -3,12 +3,18 @@ import cv2
 import os
 import json
 
+FaceCascPath = 'haarcascade_frontalface_default.xml'
+
+# Takes Img Object and RPS choice, resizes then adds the face_obj to traindata with next number as name. 
+# Adds RPS choice to labels dict.
+# Returns the path of img added
 def save2traindata(face_obj, choice):
     try:
         new_name = f'{str(max(map(lambda x: int(x.split(".png")[0]), os.listdir("FaceTrainData/images"))) + 1)}.png'   # Basically finds highest number +1 and adds .png
     except ValueError:
         new_name = '1.png'
     
+    face_obj = cv2.resize(face_obj, (50, 50))
     cv2.imwrite(f'FaceTrainData/images/{new_name}', face_obj)
 
     data = None
@@ -22,3 +28,21 @@ def save2traindata(face_obj, choice):
     
     file.close()
     return f'FaceTrainData/images/{new_name}'
+
+
+# Uses haarcascade to find largest face within image, crops img to just be face then regrayscales.
+# Pushes data through save2traindata to return img path
+def find_face(arr, choice):
+    face_cascade = cv2.CascadeClassifier(FaceCascPath)
+    img = cv2.imwrite('temp.png', arr)
+    img = cv2.imread('temp.png')
+    faces = face_cascade.detectMultiScale(img, 1.1, 4) # Might needa fiddle w the numbas
+    os.remove('temp.png')
+    
+    if len(faces) == 0:
+        return 0    # Error code?
+    else:
+        sorted(faces, key=(lambda x: x[2]*x[3]))
+        x, y, h, w = faces[0]
+        crop_img = cv2.cvtColor(img[y:y+h, x:x+w], cv2.COLOR_BGR2GRAY)
+        return save2traindata(crop_img, choice)
--- app.py
+++ app.py
@@ -10,22 +10,6 @@ import os
 
 app = Flask(__name__)
 CORS(app)
-FaceCascPath = 'haarcascade_frontalface_default.xml'
-
-def find_face(arr, choice):
-    face_cascade = cv2.CascadeClassifier(FaceCascPath)
-    img = cv2.imwrite('temp.png', arr)
-    img = cv2.imread('temp.png')
-    faces = face_cascade.detectMultiScale(img, 1.1, 4) # Might needa fiddle w the numbas
-    os.remove('temp.png')
-    
-    if len(faces) == 0:
-        return 0    # Error code?
-    else:
-        sorted(faces, key=(lambda x: x[2]*x[3]))
-        x, y, h, w = faces[0]
-        crop_img = img[y:y+h, x:x+w]
-        return rpst.save2traindata(crop_img, choice)
 
 @app.route('/face_data', methods=['GET', 'POST'])
 def test():
@@ -33,9 +17,9 @@ def test():
         return 'ok and???'
     elif request.method == "POST":
         input = request.get_json(force=True)
-        temp = find_face(np.array(input['img']), input['choice'])
+        temp = rpst.find_face(np.array(input['img']), input['choice'])
         if temp != 0:
-            face = np.asarray(Image.open(temp))
+            MLarray = np.asarray(cv2.imread(temp))
             # pump face thru neural net to get actual response
         return 'Yop'
     else:
@@ -45,4 +29,3 @@ def test():
 @app.route("/")
 def home():
     return "deez"
-    
